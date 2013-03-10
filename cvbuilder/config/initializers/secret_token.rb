21fa129075f6b2ddb8a6e55ec543e869e8959613
Damian C. Williams <damian@askagp.com>
2013-03-10 13:48:47 +0000

    First commit

---
+++ cvbuilder/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cvbuilder::Application.config.secret_token = 'd427d8a5dc613f0a8925d8e97799f4a0cd4b65c4f02f3353b0c82ff6f2915d01bb3675d756fc022ad1009cd440ebf11954b497eab87b330a6d86e5b4bd0a7b7a'
